6cea60e7c92e0802d73c2398fbe82de9c92d494d
Manav Chudasama <manavchudasama2005@gmail.com>
2024-09-20 09:57:59 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-export default function App() {
+import React from "react";
+import Dashboard from "./pages/Dashboard";
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <div>
+      <Dashboard />
+    </div>
+  );
+};
+
+export default App;
